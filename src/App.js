c5e9dab4c768306d134af6a7dea693211cba498f
rades12340 <rades12340@gmail.com>
2019-05-24 23:59:29 +0200

    basic calc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card style={{ width: "350px", margin: "100px auto", padding: "10px" }}>
+        <InputGroup md={3}>
+          <FormControl type="button" disabled className="btn" />
+        </InputGroup>
+        <ButtonGroup aria-label="Basic example" className="btn-group1">
+          <Button className="clear" variant="outline-success" md="auto">
+            CLEAR
+          </Button>
+          <Button className="btn-plus" variant="outline-success">
+            /
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup aria-label="Basic example" className="btn-group">
+          <Button variant="outline-success btn">7</Button>
+          <Button variant="outline-success btn">8</Button>
+          <Button variant="outline-success btn">9</Button>
+          <Button variant="outline-success btn">-</Button>
+        </ButtonGroup>
+        <ButtonGroup aria-label="Basic example" className="btn-group">
+          <Button variant="outline-success btn">4</Button>
+          <Button variant="outline-success btn">5</Button>
+          <Button variant="outline-success btn">6</Button>
+          <Button variant="outline-danger btn">+</Button>
+        </ButtonGroup>
+        <ButtonGroup aria-label="Basic example" className="btn-group">
+          <Button variant="outline-success btn">1</Button>
+          <Button variant="outline-success btn">2</Button>
+          <Button variant="outline-success btn">3</Button>
+          <Button variant="outline-danger btn">=</Button>
+        </ButtonGroup>
+      </Card>
     </div>
   );
 }
